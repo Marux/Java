5a615b9d0a3e9e9c9596dc5c32d11ec4ad3a63a9
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-09-18 20:00:16 -0300

    Create Cliente.java

---
+++ Prestamo/Cliente.java
@@ -0,0 +1,39 @@
+public class Cliente {
+    
+    private String nombre;
+    private int montocredito;
+    private int deuda;
+    
+    public Cliente ()
+    {
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getMontocredito() {
+        return montocredito;
+    }
+
+    public void setMontocredito(int montocredito) {
+        this.montocredito = montocredito;
+    }
+
+    public int getDeuda() {
+        return deuda;
+    }
+
+    public void setDeuda(int deuda) {
+        this.deuda = deuda;
+    }
+    
+    
+    
+    
+    
+}

7887123ee5e1a6395a2ccbab8fd934d5059ac480
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-10-19 22:39:49 -0300

    Update Principal.java

--- ProyectoFinal/Principal.java
+++ ProyectoFinal/Principal.java
@@ -1,1 +1,18 @@
 
+import java.util.Scanner;
+
+public class Principal {
+    
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner IngresoDatos = new Scanner(System.in);
+
+        System.out.println("Ingrese Nombre del colaborador: ");
+
+        System.out.println("Ingrese Rut: ");
+        
+        System.out.println("Edad: ");
+        
+    }
+    
+}

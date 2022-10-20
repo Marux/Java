acc8c1279006b3eb905510cff24fdd4c702ededc
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-10-19 22:41:33 -0300

    Create Administrador.java

---
+++ ProyectoFinal/Administrador.java
@@ -0,0 +1,20 @@
+
+public class Administrador extends Trabajador{
+    
+    private String _cargo;
+
+    public Administrador() {
+    }
+
+    public String getCargo() {
+        return _cargo;
+    }
+
+    public void setCargo(String _cargo) {
+        this._cargo = _cargo;
+    }
+    
+   
+    
+    
+}

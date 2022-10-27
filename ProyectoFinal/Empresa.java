603749ab5e858a76e4110723a45a0ad74a23f138
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-10-26 22:45:55 -0300

    Create Empresa.java

---
+++ ProyectoFinal/Empresa.java
@@ -0,0 +1,35 @@
+
+public class Empresa {
+
+    private String _nombre;
+    private String _direccion;
+    private String _ciudad;
+
+    public Empresa() {
+    }
+
+    public String getNombre() {
+        return _nombre;
+    }
+
+    public void setNombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+
+    public String getDireccion() {
+        return _direccion;
+    }
+
+    public void setDireccion(String _direccion) {
+        this._direccion = _direccion;
+    }
+
+    public String getCiudad() {
+        return _ciudad;
+    }
+
+    public void setCiudad(String _ciudad) {
+        this._ciudad = _ciudad;
+    }
+
+}

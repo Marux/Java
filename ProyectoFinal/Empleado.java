4557512966d011537e1663113c40a4cb4c0971ab
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-10-26 22:44:34 -0300

    Update and rename Trabajador.java to Empleado.java

---
+++ ProyectoFinal/Empleado.java
@@ -0,0 +1,90 @@
+
+public class Empleado {
+    
+    private String _nombre;
+    private int _rut;
+    private int _edad;
+    private String _direccion;
+    private String _comuna;
+    private String _email;
+    private String _empresa;
+    private int _telefono;
+    private String _cargo;
+
+    public Empleado() {
+    }
+
+    public String getNombre() {
+        return _nombre;
+    }
+
+    public void setNombre(String _nombre) {
+        this._nombre = _nombre;
+    }
+
+    public int getRut() {
+        return _rut;
+    }
+
+    public void setRut(int _rut) {
+        this._rut = _rut;
+    }
+
+    public int getEdad() {
+        return _edad;
+    }
+
+    public void setEdad(int _edad) {
+        this._edad = _edad;
+    }
+
+    public String getDireccion() {
+        return _direccion;
+    }
+
+    public void setDireccion(String _direccion) {
+        this._direccion = _direccion;
+    }
+
+    public String getComuna() {
+        return _comuna;
+    }
+
+    public void setComuna(String _comuna) {
+        this._comuna = _comuna;
+    }
+
+    public String getEmail() {
+        return _email;
+    }
+
+    public void setEmail(String _email) {
+        this._email = _email;
+    }
+
+    public String getEmpresa() {
+        return _empresa;
+    }
+
+    public void setEmpresa(String _empresa) {
+        this._empresa = _empresa;
+    }
+
+    public int getTelefono() {
+        return _telefono;
+    }
+
+    public void setTelefono(int _telefono) {
+        this._telefono = _telefono;
+    }
+
+    public String getCargo() {
+        return _cargo;
+    }
+
+    public void setCargo(String _cargo) {
+        this._cargo = _cargo;
+    }
+
+    
+}

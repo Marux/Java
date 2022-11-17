0feaf58b0709479e80a69fceb059cfe066901978
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-11-17 01:34:09 -0300

    Add files via upload

---
+++ ProyectoFinalSwing/src/Negocio/NegocioLogin.java
@@ -0,0 +1,17 @@
+package Negocio;
+
+import Entidades.Usuario;
+import Datos.DatoLogin;
+
+public class NegocioLogin {
+
+    public int validaLoginNeg(Usuario user) {
+        DatoLogin dl = new DatoLogin();
+
+        int respuesta = dl.validaLogin(user);
+
+        return respuesta;
+
+    }
+
+}

4e4ab790aaa67cb6a54749803ade62e964e845fc
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-11-27 18:28:04 -0300

    Update PROYECTOFINAL.sql

--- ProyectoFinalSwing/PROYECTOFINAL.sql
+++ ProyectoFinalSwing/PROYECTOFINAL.sql
@@ -9,3 +9,15 @@ numero_telefonico int(9) not null,
 nombre_cargo varchar(30) not null,
 sueldo_base int
 )
+
+CREATE TABLE AsistenciaEntrada(
+id_empleado int primary key,
+fecha date,
+hora_entrada time
+)
+
+CREATE TABLE AsistenciaSalida(
+id_empleado int primary key,
+fecha date,
+hora_salida time
+)

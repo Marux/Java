689b4e052d0c8ee4ccbf6ce300b7fd83897e4202
Victor Trimpai <victor.trimpai1987@gmail.com>
2022-11-27 01:07:20 -0300

    Add files via upload

---
+++ ProyectoFinalSwing/src/Datos/DatoLogin.java
@@ -0,0 +1,17 @@
+package Datos;
+
+import Entidades.Usuario;
+
+public class DatoLogin {
+    
+    public int validaLogin(Usuario user) {
+        
+        if (user.getUser().equals("admin") && user.getPass().equals("admin")) {
+            return 1;
+        } else {
+            return 2;
+        }
+        
+    }
+    
+}
